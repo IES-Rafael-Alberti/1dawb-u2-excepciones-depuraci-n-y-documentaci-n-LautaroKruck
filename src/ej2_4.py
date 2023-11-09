08d4f07191bcead36eebf88d746d0174460ee946
Lautaro <lkru144@g.educaand.es>
2023-11-09 13:08:57 +0100

    burbuja

--- src/ej2_4.py
+++ src/ej2_4.py
@@ -1,12 +1,37 @@
 """
+
 El algoritmo burbuja
+
 El algoritmo burbuja te permite ordenar valores de un array. Funciona revisando cada elemento con el elemanto adyacente. Si ambos elementos no están ordenados, se procede a intercambiarlos, si por el contrario los elementos ya estaban ordenados se dejan tal como estaban. Este proceso sigue para cada elemento del arreglo hasta que quede completamente ordenado.
+
 """
+def pedirNum():
+    """Función que solicita una serie de números al usuario."""
+    numeros_str = input("Ingresa una serie de números separados por espacio: ")
+    numeros = [int(num) for num in numeros_str.split()]
+    return numeros
 
-a = [8, 3, 1, 19, 14]
+def guardar_en_lista():
+    """Función que utiliza pedir_numeros y almacena los números en una lista."""
+    lista_numeros = pedir_numeros()
+    return lista_numeros
 
-n = 1
-for i in range (1,len(a)):
-    n += 1
-    for j in range (0, len(a)-n):
-        a
+def ordenarSerie(lista_numeros):
+    n = len(a)
+
+    for i in range(n):
+        # Últimos i elementos ya están ordenados, así que no necesitamos revisarlos
+        for j in range(0, n-i-1):
+            # Si el elemento actual es mayor que el siguiente, intercambiamos
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return lista_numeros
+
+print("Array ordenado:")
+print(lista_numeros)
+
+def main():
+    a
+
+if __name__ == "__main__":
+    main()
